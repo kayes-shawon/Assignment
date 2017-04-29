6667f8404bf070db50109e8f04a5ee7b4012cc98
kayes-shawon <shawon.kayes@gmail.com>
2017-04-29 16:46:31 +0600

    Display chart using chartjs

--- weather/urls.py
+++ weather/urls.py
@@ -3,7 +3,5 @@ from . import views
 from .views import TempView
 urlpatterns = [
     url(r'^temper_form/$', TempView.as_view(), name="inputform"),
-    url(r'^points/$', views.get_temperature_ajax),
-    url(r'^index/$', views.index)
-
+    url(r'^tempershow/$', views.get_temperature_show, name='aaa'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,67 +1,49 @@
 from weather.forms import TemperForm
-from django.views.generic.edit import FormView,CreateView
-from django.http import JsonResponse, HttpResponse, request
+from django.views.generic.edit import CreateView
 from django.shortcuts import render
 from .models import Temper
 
 
-
-
-# class TempView(FormView):
-#     template_name = 'temper_form.html'
-#     form_class = TemperForm
-#
-#
-#     def form_valid(self, form):
-#         return super(TempView, self).form_valid(form)
-# class TempView(FormView):
-#     template_name = 'weather/temper_form.html'
-#     form_class = TemperForm
-#     success_url = '/weather/index'
-
 class TempView(CreateView):
     template_name = 'weather/temper_form.html'
     form_class = TemperForm
-    success_url = '/weather/index'
+    success_url = '/weather/tempershow/'
+
+def get_temperature_show(request):
+
+    data = Temper.objects.all()
+    return render(request,'weather/showtemper.html',{'data':data})
+
+# def get_temperature_ajax(request):
+#     #load the model
+#     all_temperatures = Temper.objects.all()
+#
+#     dates_label = []
+#     sensor1_temp = []
+#     sensor2_temp = []
+#     sensor3_temp = []
+#     sensor4_temp = []
+#
+#     for model in all_temperatures:
+#         dates_label.append(model.date)
+#         sensor1_temp.append(model.sensor1_temp)
+#         sensor2_temp.append(model.sensor2_temp)
+#         sensor3_temp.append(model.sensor3_temp)
+#         sensor4_temp.append(model.sensor4_temp)
+#
+#     # points = [103, 93, 94, 112, 88, 90]
+#     # labels = ['A', 'B', 'C', 'D', 'E', 'F']
+#     # data = {
+#     #     'points': points,
+#     #     'labels': labels,
+#     # }
+#     data = {
+#         'dates': dates_label,
+#         'sensor1': sensor1_temp,
+#         'sensor2': sensor2_temp,
+#         'sensor3': sensor3_temp,
+#         'sensor4': sensor4_temp
+#     }
+#     return JsonResponse(data)
 
 
-def get_temperature_ajax(request):
-    #load the model
-    all_temperatures = Temper.objects.all()
-
-    dates_label = []
-    sensor1_temp = []
-    sensor2_temp = []
-    sensor3_temp = []
-    sensor4_temp = []
-
-    for model in all_temperatures:
-        dates_label.append(model.date)
-        sensor1_temp.append(model.sensor1_temp)
-        sensor2_temp.append(model.sensor2_temp)
-        sensor3_temp.append(model.sensor3_temp)
-        sensor4_temp.append(model.sensor4_temp)
-
-    # points = [103, 93, 94, 112, 88, 90]
-    # labels = ['A', 'B', 'C', 'D', 'E', 'F']
-    # data = {
-    #     'points': points,
-    #     'labels': labels,
-    # }
-    data = {
-        'dates': dates_label,
-        'sensor1': sensor1_temp,
-        'sensor2': sensor2_temp,
-        'sensor3': sensor3_temp,
-        'sensor4': sensor4_temp
-    }
-
-    return JsonResponse(data)
-
-
-def display_chart(request):
-    pass
-
-
-def index(request):
-    return render(request=request, template_name='weather/index.html')
